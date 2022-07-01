5ce1b7a3b3f8b030d44ffd373d3f168089db1e0d
siyambalaji <siyambalaji10@gmail.com>
2022-07-01 10:14:50 +0530

    quiz CRUD is completed

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,6 +1,8 @@
 class QuizzesController < ApplicationController
     before_action :authenticate_user!
     def index
+        @quizzes = Quiz.all
+        puts @quizzes.inspect
     end
     def new
         @quiz = Quiz.new
@@ -12,7 +14,26 @@ class QuizzesController < ApplicationController
         end
         redirect_to root_path
     end
+    def edit
+        find_quiz
+    end
+    def update
+        find_quiz
+        @quiz.update(quiz_params)
+        if @quiz.save!
+            puts "Saved in database"
+        end
+        redirect_to root_path
+    end
+    def destroy
+        find_quiz
+        @quiz.destroy!
+        redirect_to root_path
+    end
     private
+    def find_quiz
+        @quiz = Quiz.find_by_id(params[:id])
+    end
     def quiz_params
         params.require(:quiz).permit(:title, :description,:no_of_questions,:individual_mark,:cut_off_mark,:negative_mark,:user_id)
     end
